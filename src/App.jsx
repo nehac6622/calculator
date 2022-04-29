8d9abbc7dc66f75f8abcd02f410bee846a461e8f
nehac6622 <nehac6622@gmail.com>
2022-04-29 11:42:29 +0530

    edited

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <div>
 
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos minima, assumenda impedit molestiae vel officiis tempore recusandae sed quasi nobis ut est eos alias possimus culpa veritatis. Sequi molestias, debitis consequatur voluptatum voluptate facilis asperiores rem ipsum perferendis labore nemo ea repellendus earum libero maxime odio necessitatibus. Nisi, quod tenetur!
+        neha
       </div>
     </div>
   )
