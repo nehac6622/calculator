cd4f9225dd8d309c945a57f7309a6a03d370644c
nehac6622 <nehac6622@gmail.com>
2022-04-29 17:01:08 +0530

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,18 @@ function App() {
 
 
   return (
-    <div className="App">
-      <div>
-
-        tejas
-      </div>
+    <div>
+        <div className="bg-amber-400 h-24 w-56 mx-auto my-12 rounded-lg">
+          <div className="ml-auto max-w-max">
+            fabvbkbve
+          </div>
+        </div>
+        <div className="flex flex-1 max-w-max space-x-8 mx-auto">
+        <div>7
+        </div>
+        <div>8</div>
+        <div>9</div>
+        </div>
     </div>
   )
 }
