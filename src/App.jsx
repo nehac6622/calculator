49f3c4428b2521b2db77b7be3b65a59d341b44cc
nehac6622 <nehac6622@gmail.com>
2022-04-29 11:25:27 +0530

    pahili commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div className="App">
+      <div>
+
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dignissimos minima, assumenda impedit molestiae vel officiis tempore recusandae sed quasi nobis ut est eos alias possimus culpa veritatis. Sequi molestias, debitis consequatur voluptatum voluptate facilis asperiores rem ipsum perferendis labore nemo ea repellendus earum libero maxime odio necessitatibus. Nisi, quod tenetur!
+      </div>
+    </div>
+  )
+}
+
+export default App
