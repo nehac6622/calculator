fe234b232485258545f2ed2a661ca183b78805aa
nehac6622 <nehac6622@gmail.com>
2022-04-29 17:06:41 +0530

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,15 @@ function App() {
 
   return (
     <div>
-        <div className="bg-amber-400 h-24 w-max sm:w-56 mx-auto my-12 rounded-lg">
-          <div className="ml-auto max-w-max">
+        <div className="bg-amber-400 h-24 w-screen sm:w-56 mx-auto my-12 rounded-lg">
+          <div className="ml-auto text-xl max-w-max">
             fabvbkbve
           </div>
         </div>
         <div className="flex flex-1 max-w-max space-x-8 mx-auto">
-        <div className="text-lg">7</div>
-        <div className="text-lg">8</div>
-        <div className="text-lg">9</div>
+        <div className="text-xl">7</div>
+        <div className="text-xl">8</div>
+        <div className="text-xl">9</div>
         </div>
     </div>
   )
